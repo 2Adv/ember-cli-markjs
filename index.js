61513266d914e23b845d2ce1ad45c37c0c88051d
Marcelo Fernandes <marcelo.fernandes@sou2adv.in>
2017-09-18 17:04:12 -0300

    va

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-cli-markjs',
   
   isDevelopingAddon() {
-    return true;
+    return false;
   },
   included: function(app) {
     app.import('bower_components/mark.js/dist/jquery.mark.js');
